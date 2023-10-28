045a34e7d3c17580c1389cbb2dbb1ea291578d06
MiloszPapierz1607 <milosz.papierz@student.hogent.be>
2023-10-28 11:34:47 +0200

    Remove unused imports in mainactivity and theme file

--- app/src/main/java/com/example/fieldfavorites/MainActivity.kt
+++ app/src/main/java/com/example/fieldfavorites/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -15,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fieldfavorites.ui.component.LeagueCard
 import com.example.fieldfavorites.ui.theme.FieldFavoritesTheme
