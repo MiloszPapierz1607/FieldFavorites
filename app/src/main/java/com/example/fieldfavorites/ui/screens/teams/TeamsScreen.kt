389f9f1b104ab8f671b42cbf5246944ed090fe85
MiloszPapierz1607 <milosz.papierz@student.hogent.be>
2023-12-15 16:33:18 +0100

    Fix tests

--- app/src/main/java/com/example/fieldfavorites/ui/screens/teams/TeamsScreen.kt
+++ app/src/main/java/com/example/fieldfavorites/ui/screens/teams/TeamsScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -57,7 +56,7 @@ object TeamsDestination : NavigationDestination {
     val routeWithArgs = "$route/{$itemIdArg}"
 }
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3WindowSizeClassApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TeamsScreen(
     deviceType: DeviceType,
